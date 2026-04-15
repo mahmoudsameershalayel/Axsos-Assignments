c66e9f6e76a75887d3f30689e416febb795a42d1
mas4tech <ahmedoshkaa@gmail.com>
2026-04-15 17:00:04 +0300

    for-loops-basic1

---
+++ python_stack/_python/python_fundamentals/For_Loops_Basic1/for_loop_basic1.py
@@ -0,0 +1,34 @@
+#1. Basic 
+for i in range(151):
+    print(i)
+    
+#2. Multiples of Five 
+for i in range(5 , 1001 , 5):
+    print(i)
+    
+#3. Counting, the Dojo Way
+for i in range(1, 101):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 == 0:
+        print("Coding")
+    else:
+        print(i)
+#4. Whoa. That Sucker's Huge
+sum = 0
+for i in range(1 ,500000,2):
+    sum += i;
+print("The sum is : " + str(sum))
+
+#5. Countdown by Fours
+for i in range(2018 , 0 , -4):
+    print(i)
+
+#6. Flexible Counter 
+lowNum = 2
+highNum = 9
+mult = 3
+
+for i in range(lowNum, highNum + 1):
+    if i % mult == 0:
+        print(i)
