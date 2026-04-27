853966ec4be5e8096f801983e34daea6e37638b7
mas4tech <ahmedoshkaa@gmail.com>
2026-04-27 11:32:49 +0300

    routing-assignment

---
+++ python_stack/flask/flask_fundamentals/understanding_routing/main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route("/Champion")
+def champion():
+    return "Champion!"
+
+@app.route("/say/<name>")
+def say_hi(name):
+    return f"Hi {name}!"
+
+@app.route("/repeat/<int:num>/<word>")
+def repeat_word(num , word):
+    return word * num
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Sorry! No response. Try again."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
