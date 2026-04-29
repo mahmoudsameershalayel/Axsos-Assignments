9e7d56f6ce22243df9f82bb52071a4d2912d335d
MahmoudShalayel <mahmoudsameer059@gmail.com>
2026-04-29 12:08:37 +0300

    playground-assignment

--- python_stack/flask/flask_fundamentals/playground/main.py
+++ python_stack/flask/flask_fundamentals/playground/main.py
@@ -3,8 +3,16 @@ app = Flask(__name__)
 
 @app.route("/play")
 def play():
-    return render_template("index.html")
+    return render_template("index.html" , card_nums = 3 , card_color = "aqua")
 
+@app.route("/play/<int:nums>")
+def play_cards(nums):
+    return render_template("index.html" , card_nums = nums, card_color = "aqua")
+
+@app.route("/play/<int:nums>/<color>")
+def play_cards_with_colors(nums, color):
+    return render_template("index.html" , card_nums = nums, card_color = color)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
